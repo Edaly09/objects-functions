13be675176c5de824d2308f372dd74652e4e762c
Edaly09 <edalyminthe@gmail.com>
2022-06-23 15:20:04 +0200

    add new

--- index.js
+++ index.js
@@ -17,3 +17,37 @@
 //     console.log("So cute ! ", cat.isCute)
 // }
 
+// ## 02 - Combine
+
+// const cat2 = {
+//     name : "felix",
+//     Age : 10,
+//     isCute : false,
+  
+  
+                            
+// }
+
+// let cats = [ cat2, cat]
+
+//      console.log(cats[1].Age)
+//      console.log(cats[0].isCute)
+
+// ## 03 - Even
+// const checkIfEven = (num) => {
+//     if(num % 2 === 0){
+//         console.log("even")
+//     }
+//     else {
+//         console.log("odd")
+//     }
+
+// }
+
+// checkIfEven(2)
+// checkIfEven(19)
+// checkIfEven(4)
+// checkIfEven(0)
+// checkIfEven(747)
+// checkIfEven(67)
+
