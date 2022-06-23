79412a4cb977bc8ec74ed1adc98d9ea2b2ab30dc
Edaly09 <edalyminthe@gmail.com>
2022-06-23 14:42:58 +0200

    objets and functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// ## 01 - Object
+
+
+// const cat = {
+//     name : "grefield",
+//     Age : 3,
+//     isCute : true,
+
+  
+// }
+// console.log(cat)
+
+// console.log(cat.Age)
+// const isCute = true
+
+// if(isCute){
+//     console.log("So cute ! ", cat.isCute)
+// }
+
