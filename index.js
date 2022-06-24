5c9d7cc982e7628c9833eb9767f9dd77978cd36f
Edaly09 <edalyminthe@gmail.com>
2022-06-24 23:00:00 +0200

    niveau 6

--- index.js
+++ index.js
@@ -77,23 +77,43 @@
 
 // ## 05 - Add Up
 
-const addUp = (num) =>{
-    let res = 0
-    for(let i = 1; i <= num; i++){
+// const addUp = (num) =>{
+//     let res = 0
+//     for(let i = 1; i <= num; i++){
 
-    res = res + i
+//     res = res + i
 
-    }
-    console.log(res)
-}
-addUp(12)
-
-// - Créez une fonction `addUp` qui reçoit un paramètre `num`
-// - À l'aide d'une boucle, ajouter les chiffres de 1 à `num`
-// - Appelez votre fonction avec le chiffre 12 et vérifiez que vous obtenez 78
+//     }
+//     console.log(res)
+// }
+// addUp(12)
 
 // ## 06 - Time
+
+// const format = (num) => {
+// const heures = Math.floor(num / 3600)
+//     const Firstminutes = (num % 3600)
+//     const minutes = Math.floor(Firstminutes / 60)
+//     const secondes = (Firstminutes % 60)
+
+//     console.log(`${heures}:${minutes}:${secondes}`)
+// }
+
+// format(3700)
+
 
 // - Créez une fonction `format` qui reçoit un paramètre `num` qui représente des secondes
 // - Faites en sorte que la fonction retourne un format `heures : minutes : secondes`
 // - Appelez votre fonction avec l'argument `3700` et vérifiez que vous obtenez `1:1:40`
+
+
+// ## 07 - Password generation
+
+
+
+// - Créez une fonction `generatePassword` qui reçoit un paramètre `num`
+// - La fonction génère un mot de passe aléatoire, avec autant de lettres que la valeur de `num`
+// - Le mot de passe contiendra uniquement des lettres majuscules
+// - Si `num` est plus petit que 6 ou plus grand que 15, la fonction retourne "error"
+
+// Indice: utiliser la table ascii
